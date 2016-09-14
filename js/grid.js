ec6be236fde7856348b732b33a9c11a0001febf1
HDavis1487 <hdavis387@gmail.com>
2016-09-13 21:33:35 -0400

    Second Commit

---
+++ js/grid.js
@@ -0,0 +1,13 @@
+// Masonry Grid
+$('.grid').masonry({
+  itemSelector: '.item',
+  columnWidth: 180,
+  gutter: 20
+});
+
+
+// $('.item').each(function(i){
+//   setTimeout(function(){
+//     $('.item').eq(i).addClass('is-visible');
+//   }, 200 * i);
+// });
